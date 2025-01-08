f685f02423d8281f24e423c0147fbfca34a73978
Andrey <andrey.mikhaylov.n@gmail.com>
2025-01-08 15:32:16 +0300

    main

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+    pass
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+"""
+2024/02/18 00:00|Домашнее задание по теме "CRUD Запросы: Get, Post, Put Delete."
+Цель: выработать навык работы с CRUD запросами.
+
+Задача "Имитация работы с БД":
+Создайте новое приложение FastAPI и сделайте CRUD запросы.
+Создайте словарь users = {'1': 'Имя: Example, возраст: 18'}
+Реализуйте 4 CRUD запроса:
+get запрос по маршруту '/users', который возвращает словарь users.
+post запрос по маршруту '/user/{username}/{age}', который добавляет в словарь по максимальному по значению ключом значение строки "Имя: {username}, возраст: {age}". И возвращает строку "User <user_id> is registered".
+put запрос по маршруту '/user/{user_id}/{username}/{age}', который обновляет значение из словаря users под ключом user_id на строку "Имя: {username}, возраст: {age}". И возвращает строку "The user <user_id> is updated"
+delete запрос по маршруту '/user/{user_id}', который удаляет из словаря users по ключу user_id пару.
+Выполните каждый из этих запросов по порядку. Ответы должны совпадать:
+1. GET '/users'
+{
+"1": "Имя: Example, возраст: 18"
+}
+2. POST '/user/{username}/{age}' # username - UrbanUser, age - 24
+"User 2 is registered"
+3. POST '/user/{username}/{age}' # username - NewUser, age - 22
+"User 3 is registered"
+4. PUT '/user/{user_id}/{username}/{age}' # user_id - 1, username - UrbanProfi, age - 28
+"User 1 has been updated"
+5. DELETE '/user/{user_id}' # user_id - 2
+"User 2 has been deleted"
+6. GET '/users'
+{
+"1": "Имя: UrbanProfi, возраст: 28",
+"3": "Имя: NewUser, возраст: 22"
+}
+Пример результата выполнения программы:
+Как должен выглядеть Swagger:
+
+
+Примечания:
+Не забудьте написать валидацию для каждого запроса, аналогично предыдущему заданию.
+Файл module_16_3.py загрузите на ваш GitHub репозиторий. В решении пришлите ссылку на него.
+"""
